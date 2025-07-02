9d642f9b1cb66e8a111d4fa22baa4b2a6384485b
Chris <150510823+ChrisF1976@users.noreply.github.com>
2025-07-02 10:52:05 +0200

    Update MMM-EventSearch.js

--- MMM-EventSearch.js
+++ MMM-EventSearch.js
@@ -6,11 +6,12 @@ Module.register("MMM-EventSearch", {
     updateInterval: 12*60*60*1000, // every 12 hours
     hl: "de",
     gl: "de",
-    maxResults: 5, //maximum number of shown results
+    maxResults: 5, // maximum number of shown results
+    rotateMoreEvents: true, // new option to enable rotation
+    rotateInterval: 60*1000, // new option for rotation interval (1 minute)
     googleDomain: "google.de",
-    moduleWidth: "400px", // Add configurable width
+    moduleWidth: "400px",
   },
-
 
   getStyles: function () {
     return ["MMM-EventSearch.css"];
@@ -18,17 +19,38 @@ Module.register("MMM-EventSearch", {
 
   start: function () {
     this.events = [];
+    this.currentRotationIndex = 0; // Track current rotation position
     this.sendSocketNotification("FETCH_EVENTS", this.config);
 
     // Refresh events periodically
     setInterval(() => {
       this.sendSocketNotification("FETCH_EVENTS", this.config);
     }, this.config.updateInterval);
+
+    // Setup rotation if enabled
+    if (this.config.rotateMoreEvents) {
+      setInterval(() => {
+        this.rotateEvents();
+      }, this.config.rotateInterval);
+    }
+  },
+
+  rotateEvents: function() {
+    if (!this.events || this.events.length <= this.config.maxResults) {
+      return; // No need to rotate if we don't have enough events
+    }
+    
+    this.currentRotationIndex += this.config.maxResults;
+    // Wrap around if we've reached the end
+    if (this.currentRotationIndex >= this.events.length) {
+      this.currentRotationIndex = 0;
+    }
+    this.updateDom();
   },
 
   getDom: function () {
     const wrapper = document.createElement("div");
-    wrapper.style.width = this.config.moduleWidth; // Set the module width
+    wrapper.style.width = this.config.moduleWidth;
     wrapper.classList.add("MMM-EventSearch");
 
     if (!this.events || this.events.length === 0) {
@@ -38,9 +60,22 @@ Module.register("MMM-EventSearch", {
 
     const table = document.createElement("table");
     table.className = "eventTable";
-    
-    // Apply maxResults limit here
-    const eventsToShow = this.events.slice(0, this.config.maxResults);
+
+    // Get the current slice of events to show
+    let eventsToShow = [];
+    if (this.config.rotateMoreEvents && this.events.length > this.config.maxResults) {
+      // Handle wrapping around the array
+      const endIndex = this.currentRotationIndex + this.config.maxResults;
+      if (endIndex > this.events.length) {
+        eventsToShow = this.events.slice(this.currentRotationIndex)
+          .concat(this.events.slice(0, endIndex % this.events.length));
+      } else {
+        eventsToShow = this.events.slice(this.currentRotationIndex, endIndex);
+      }
+    } else {
+      // Normal case - just show first maxResults events
+      eventsToShow = this.events.slice(0, this.config.maxResults);
+    }
 
     eventsToShow.forEach((event) => {
       const row1 = document.createElement("tr");
@@ -84,9 +119,8 @@ Module.register("MMM-EventSearch", {
   socketNotificationReceived: function (notification, payload) {
     if (notification === "EVENTS_FETCHED") {
       this.events = payload;
+      this.currentRotationIndex = 0; // Reset rotation when new events arrive
       this.updateDom();
     }
   },
-
-
 });
